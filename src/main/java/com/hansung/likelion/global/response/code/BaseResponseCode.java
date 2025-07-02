766c17123e4c64b23bda1db9c341e87338d730c9
woodgeon <ad4086@naver.com>
2025-07-02 15:21:50 +0900

    feat: 공통 응답 코드 추가

---
+++ src/main/java/com/hansung/likelion/global/response/code/BaseResponseCode.java
@@ -0,0 +1,10 @@
+package com.hansung.likelion.global.response.code;
+
+/***
+ * 공통 응답 코드 인터페이스(틀)
+ */
+public interface BaseResponseCode {
+    String getCode();
+    int getHttpStatus();
+    String getMessage();
+}
---
+++ src/main/java/com/hansung/likelion/global/response/code/SuccessResponseCode.java
@@ -0,0 +1,16 @@
+package com.hansung.likelion.global.response.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import static com.hansung.likelion.global.constant.StaticValue.*;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessResponseCode implements BaseResponseCode {
+    SUCCESS_OK("SUCCESS_200", OK, "호출에 성공하였습니다."),
+    SUCCESS_CREATED("SUCCESS_201", CREATED, "호출에 성공하였습니다.");
+    private final String code;
+    private final int httpStatus;
+    private final String message;
+}
